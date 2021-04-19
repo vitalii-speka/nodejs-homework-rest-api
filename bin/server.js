3045a953ea95acc8eb56debc77a818870fbc5dd7
Vitalii Speka <spekava@gmail.com>
2021-04-19 22:22:46 +0300

    require moongose

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose')
 const app = require('../app')
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
+
+async function start() {
+  try {
+    await mongoose.connect('mongodb+srv://vitaliiSpeka:vitall1987@clustershw.e3xi6.mongodb.net/contacts', {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    })
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
