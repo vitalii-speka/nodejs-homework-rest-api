d222aec6f073cd35e7e364b66610752d0b2b25bf
Vitalii Speka <spekava@gmail.com>
2021-05-06 21:12:48 +0300

    step 1 = end

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const boolParser = require('express-query-boolean')
+
 const contactsRouter = require('./routes/contacts')
 const usersRouter = require('./routes/users')
 const { HttpCode } = require('./helper/constants')
-const rateLimit = require('express-rate-limit')
-const boolParser = require('express-query-boolean')
 
 const app = express()
 
@@ -15,6 +16,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(helmet())
 app.use(logger(formatsLogger))
+app.use(express.static('public'))
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 1 hour window
@@ -27,9 +29,18 @@ const limiter = rateLimit({
     })
   },
 })
+
 app.use(limiter)
-app.use(cors())
-app.use(express.json())
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: HttpCode.NO_CONTENT,
+  }),
+)
+
+app.use(express.json({ limit: 100000 }))
 app.use(boolParser())
 
 app.use('/api/users', usersRouter)
